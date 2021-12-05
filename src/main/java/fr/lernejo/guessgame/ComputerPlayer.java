bd0cd303bf5e54ef99d7718f3cd38b34fefb5fa4
reuben-alt <rubenhattab@gmail.com>
2021-12-05 01:20:48 +0100

    ComputerPlayer

---
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -0,0 +1,25 @@
+package fr.lernejo.guessgame;
+
+import java.security.SecureRandom;
+
+public class ComputerPlayer implements Player{
+    private Long lastGuess;
+    private boolean response;
+
+    @Override
+    public long askNextGuess() {
+        if (lastGuess == null) {
+            lastGuess = new SecureRandom().nextLong(0, Long.MAX_VALUE);
+        }
+        lastGuess = response ? lastGuess * 2 : lastGuess / 2; // FIXME
+        return lastGuess;
+    }
+
+    /**
+     * Called by {@link Simulation} to inform that the previous guess was lower or greater that the number to find.
+     */
+    @Override
+    public void respond(boolean lowerOrGreater) {
+        response = lowerOrGreater;
+    }
+}
